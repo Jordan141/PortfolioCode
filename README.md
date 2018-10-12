a3944962a2d0be8832f78c3ef198443427662c52
Jordan Moss <jmoss636@gmail.com>
2018-10-12 06:57:34 +0100

    Initial navbar layout and styling

--- README.md
+++ README.md
@@ -3,26 +3,8 @@
 This repository contains source code for my [portfolio](https://jordan141.github.io)
 
 ### Technologies used:
-***
- Name | Version
-------- | --------
-React | v14.3
-Babel | v6.2.0
-JavaScript | EcmaScript 6
-Webpack | v1.12.9
-&nbsp;
-### Main Features:
-***
-* Geo-localization for automatic page translating.
-* ReactJS to smoothly transition between languages.
-* Links and small tidbits of information about my more noteworthy projects.
+ReactJS
 
-#### To Do List [Development Purposes]
-***
-* Revise and continue translations for most common languages. (DE, IE, FR, ES)<br>
-**Important note: I will not be translating for every language.**
-* Correct styling error when the captions information exceed two lines.
-* Adjust color scheme for easier readability of banner.
-* Extend AboutMe section with more content.
-* Provide sources of all images used in the website. All are attained via royalty free image sites.
-* Fix automatic language finder
+
+### Self-imposed challenges:
+- No Bootstrap
---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import logo from '../assets/img/logo.png'
+
+class Navbar extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div className='navbar'>
+                <header>
+                    <nav id='nav' className = 'center grid-14'>
+                        <div className='fonts-display clearfix'>
+						<p className ='text-left alpha'><a href='/'>Home</a></p>
+                        <p className ='text-left'><a href='/work'>Work</a></p>
+						<p className ='text-left'><a href='/about'>About</a></p>
+						<p className ='logo mq-wide-i-block mq-mid-i-block mq-small-i-block'>
+							<a href='/' title='Built by Jordan Moss'>
+								<img src={logo} alt='Built by Jordan Moss' alt='logo'/>
+							</a>
+						</p>
+                        <p className ='text-right'><a href='/feed'>Feed</a></p>
+						<p className ='text-right'><a href='/jobs'>Jobs</a></p>
+						<p className ='text-right omega'><a href='/contact'>Contact</a></p>
+                        </div>
+                    </nav>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Navbar
