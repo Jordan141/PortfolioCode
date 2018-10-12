c55e0cf8247b14baeaee2f0ebda8f508711fe5fa
Jordan Moss <jmoss636@gmail.com>
2018-10-12 09:34:50 +0100

    link fixes and css comments

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,15 +12,15 @@ class Navbar extends Component {
                     <nav id='nav' className = 'center grid-14'>
                         <div className='fonts-display clearfix'>
 						<p className ='text-left alpha'><a href='/'>Home</a></p>
-                        <p className ='text-left'><a href='/work'>Work</a></p>
+                        <p className ='text-left'><a href='/skills'>Skills</a></p>
 						<p className ='text-left'><a href='/about'>About</a></p>
-						<p className ='logo mq-wide-i-block mq-mid-i-block mq-small-i-block'>
+						<p className ='logo'>
 							<a href='/' title='Built by Jordan Moss'>
 								<img src={logo} alt='Built by Jordan Moss' alt='logo'/>
 							</a>
 						</p>
                         <p className ='text-right'><a href='/feed'>Feed</a></p>
-						<p className ='text-right'><a href='/jobs'>Jobs</a></p>
+						<p className ='text-right'><a href='/projects'>Projects</a></p>
 						<p className ='text-right omega'><a href='/contact'>Contact</a></p>
                         </div>
                     </nav>
