660c2cc346ed9f64dcf614c16022e15f985c0cb2
Jordan Moss <jmoss636@gmail.com>
2018-10-12 04:48:05 +0100

    Installing CRA

--- src/index.js
+++ src/index.js
@@ -1,42 +1,12 @@
-import React, { Component } from 'react';
-import ReactDOM, {render} from 'react-dom';
-import Banner from './components/banner'
-import Navbar from './components/navbar'
-import AboutMe from './components/aboutme'
-import Portfolio from './components/portfolio'
-import Contact from './components/contact'
-import Freecodecamp from './components/freecodecamp'
-import config from './config'
-let myLocalization = navigator.language || navigator.userLanguage;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    if(myLocalization === 'en-US'){
-      myLocalization = 'en'
-    }
-    myLocalization = myLocalization.toUpperCase();
-    this.state = {
-        geo: myLocalization
-     }
-     this.onLanguageChange = this.onLanguageChange.bind(this);
-  }
-  onLanguageChange(input = 'EN'){
-    this.setState({geo: input})
-  }
+ReactDOM.render(<App />, document.getElementById('root'));
 
-  render() {
-    const {navbar, banner, about, language, contact, portfolio, footer} = config[this.state.geo]
-    return (
-      <div>
-        <Navbar brand={navbar.brand} geo={language} languages={config.languages} onLanguageChange={this.onLanguageChange} tiles={navbar.tiles}/>
-        <Banner sub={banner.sub} title={banner.title} />
-        <AboutMe data={about}/>
-        <Portfolio data={portfolio} />
-        <Contact desc={contact.desc} title={contact.title}/>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.querySelector('.wrapper'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
