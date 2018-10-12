09c8c9d7b38dfc4e9bf8388fb3eedc3cf04f6de8
Jordan Moss <jmoss636@gmail.com>
2018-10-12 10:11:50 +0100

    Add landing page to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Navbar from './components/navbar'
+import LandingPage from './components/landing'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='wrapper'>
         <Navbar />
+        <LandingPage />
       </div>
-    );
+    )
   }
 }
 
